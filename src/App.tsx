76a5398948e6625e3f09c1ffdfc19e1754e538fe
Maulik Pipaliya <maulik.pipaliya@gmail.com>
2021-06-11 13:40:17 +0530

    Created Types and Interfaces

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          My Weather App
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/types.ts
@@ -0,0 +1,60 @@
+export const GET_WEATHER = "GET_WEATHER";
+
+export interface IWeather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+export interface ITempMain {
+  feels_like: number;
+  humidity: number;
+  pressure: number;
+  temp: number;
+  temp_max: number;
+  temp_min: number;
+}
+export interface ICoords {
+  lon: number;
+  lat: number;
+}
+
+export interface IWind {
+  speed: number;
+  deg: number;
+}
+
+//Main Interface
+export interface IWeatherData {
+  cod: number;
+  coord: ICoords;
+  dt: number;
+  id: number;
+  main: ITempMain;
+  name: string;
+  sys: {
+    country: string;
+    id: number;
+    sunrise: number;
+    sunset: number;
+    type: number;
+  };
+  timezone: number;
+  weather: IWeather[];
+  wind: IWind;
+}
+
+export interface IWeatherError {
+  cod: number;
+  message: string;
+}
+
+
+//Action Interfaces
+interface IGetWeatherAction{
+    type: typeof GET_WEATHER;
+    payload: IWeatherData;
+}
+
+export type IWeatherAction = IGetWeatherAction;
